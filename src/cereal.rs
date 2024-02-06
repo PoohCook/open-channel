66671cf28d9013f19a5dff45233eb4c3fd2075da
PoohCook <PoohCook@gmail.com>
2024-02-06 14:55:58 +0900

    documented and refactored Cereal

--- src/cereal.rs
+++ src/cereal.rs
@@ -1,39 +1,71 @@
 use std::collections::HashMap;
 
-/// A Hub for serializing and deserializing Cereal Flavors into a Package Stream
+/// A Hub for packing and unpacking Cereal Boxes into a Cereal Stream
 ///
 /// # Examples
 ///
 /// ```
-/// use open_channel::cereal::Packager;
+///   use open_channel::cereal::CerealStream;
 ///
-/// let mut packager = Packager::new();
+///   let mut stream = CerealStream::new();
+///   assert_eq!(stream.is_empty(), true);
+///
+///   stream.push_bytes(&[1,2,3]);
+///   assert_eq!(stream.is_empty(), false);
+///
+///   let out = stream.pop_bytes(2);
+///   assert_eq!(out, [1,2].to_vec());
+///   assert_eq!(stream.is_empty(), false);
+///
+///   let out = stream.pop_byte();
+///   assert_eq!(out, 3);
+///   assert_eq!(stream.is_empty(), true);
 /// ```
 pub struct CerealStream(Vec<u8>);
 
 impl CerealStream {
+    /// Creates a new [`CerealStream`].
     pub fn new() -> Self {
         Self(Vec::new())
     }
 
+    /// Returns a reference to the get vec of this [`CerealStream`].
     pub fn get_vec(&self) -> &Vec<u8> {
         &self.0
     }
 
+    /// Returns true if the stream is empty [`CerealStream`].
     pub fn is_empty(&self) -> bool {
         self.get_vec().len() == 0
     }
 
+    /// Returns the a singel byte from the stream.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the stream is empty.
     pub fn pop_byte(&mut self) -> u8 {
         self.0.drain(0..1).next().unwrap()
     }
 
+    /// Returns the specified number of bytes from the stream.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the stream does not conatin enough bytes.
     pub fn pop_bytes(&mut self, num_bytes: usize) -> Vec<u8> {
         self.0.drain(0..num_bytes).collect()
     }
 
+    /// pushes the specified bytes into the stream.
     pub fn push_bytes(&mut self, bytes: &[u8]) {
         self.0.extend(bytes)
+    }
+}
+
+impl Default for CerealStream {
+    fn default() -> Self {
+        Self::new()
     }
 }
 
@@ -75,6 +107,8 @@ pub trait CerealBox {
 /// ```
 pub struct Packager {
     map: HashMap<u8, Box<dyn CerealBox>>,
+    stream: CerealStream,
+
 }
 
 impl Packager {
@@ -82,28 +116,34 @@ impl Packager {
     /// Creates a new [`Packager`].
     pub fn new() -> Self {
         Self {
-            map: HashMap::new()
+            map: HashMap::new(),
+            stream: CerealStream::new()
         }
     }
 
-    /// Adds a Ceareal Flavor to the Packager.
+    /// Returns the true if the contained stream is empty.
+    pub fn is_empty(&self) -> bool {
+        self.stream.is_empty()
+    }
+
+    /// Adds a Cereal Box to the Packager.
     ///
     /// # Panics
     ///
-    /// Panics if a cereal flavor has the same id key as a previously added flavor.
-    pub fn add_flavor(&mut self, flavor: Box<dyn CerealBox>){
-        let id = flavor.get_id();
+    /// Panics if a cereal cereal_box has the same id key as a previously added cereal_box.
+    pub fn add_flavor(&mut self, cereal_box: Box<dyn CerealBox>){
+        let id = cereal_box.get_id();
         if self.map.contains_key(&id) {
-            panic!("Error adding flavor ot Packager! Multiple flavors have id: {}", id);
+            panic!("Error adding cereal_box ot Packager! Multiple flavors have id: {}", id);
         }
-        self.map.insert(id, flavor);
+        self.map.insert(id, cereal_box);
     }
 
     /// unpack a ceral box into a cereal stream.
-    pub fn unpack(&self, msg: &dyn CerealBox, out: &mut CerealStream){
+    pub fn unpack(&mut self, msg: &dyn CerealBox){
         let id = msg.get_id();
-        out.push_bytes(&[id]);
-        msg.pour_out(out);
+        self.stream.push_bytes(&[id]);
+        msg.pour_out(&mut self.stream);
     }
 
     /// pack a ceral box from the cereal stream.
@@ -112,11 +152,11 @@ impl Packager {
     ///
     /// This function will return an error if the cereal stream does not
     /// have enough bytes in it.
-    pub fn pack(&mut self, source: &mut CerealStream) -> Result<(), String> {
-        let id = source.pop_byte();
+    pub fn pack(&mut self) -> Result<(), String> {
+        let id = self.stream.pop_byte();
         let mut result: Result<(), String> = Ok(());
         self.map.entry(id).and_modify(|msg| {
-            result = msg.pour_in(source);
+            result = msg.pour_in(&mut self.stream);
         });
         result
     }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod boxes;
 use boxes::{*};
-use open_channel::cereal::{CerealStream, Packager};
+use open_channel::cereal::Packager;
 use open_channel::serial_params::{Parity, StopBits};
 
 impl Ping{
@@ -40,7 +40,6 @@ impl SerialParams{
 }
 
 
-
 fn create_packger() -> Packager {
     let mut packager = Packager::new();
     packager.add_flavor(Box::new(Ping::default()));
@@ -58,55 +57,51 @@ fn main() {
 
     let mut packager = create_packger();
 
-    let mut serial = CerealStream::new();
-    packager.unpack(&Ping::default(), &mut serial);
-    packager.unpack(&Pong::default(), &mut serial);
-    packager.unpack(&VersionQuery::default(), &mut serial);
+    packager.unpack(&Ping::default());
+    packager.unpack(&Pong::default());
+    packager.unpack(&VersionQuery::default());
     packager.unpack(&VersionData{
         major: 1,
         minor: 2,
         maintenance: 3,
         build: 4,
-    }, &mut serial);
+    });
     packager.unpack(&VersionData{
         major: 4,
         minor: 1,
         maintenance: 9,
         build: 3,
-    }, &mut serial);
+    });
     packager.unpack(&SerialParams{
         channel: 1,
         baud: 19200,
         parity: Parity::Even,
         stop: StopBits::One
-    }, &mut serial);
+    });
     packager.unpack(&AdcQuery{
         channel: 3,
         length:4,
         increment_usec: 1500
-    }, &mut serial);
+    });
     packager.unpack(&AdcData{
         channel: 7,
         data: [1024, 1999, 0, -800, -900].to_vec()
-    }, &mut serial);
+    });
     packager.unpack(&SerialParams{
         channel: 2,
         baud: 9600,
         parity: Parity::Odd,
         stop: StopBits::Two
-    }, &mut serial);
+    });
     packager.unpack(&SerialParams{
         channel: 3,
         baud: 4800,
         parity: Parity::None,
         stop: StopBits::One
-    }, &mut serial);
+    });
 
-    while !serial.is_empty(){
-        packager.pack(&mut serial).unwrap();
+    while !packager.is_empty(){
+        packager.pack().unwrap();
     }
-
-    // assert_eq!(0, serial.get_vec().len());
-
 
 }
